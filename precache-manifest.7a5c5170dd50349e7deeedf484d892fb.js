fb12396acb81768dbff13d93a181c02b7f9f6883
unknown <marco.fernandes.inf10@hotmail.com>
2020-11-17 18:53:16 +0000

    Updates

---
+++ precache-manifest.7a5c5170dd50349e7deeedf484d892fb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e62df5b924358fcf1a77ceedcc8d72",
+    "url": "/powerbet/index.html"
+  },
+  {
+    "revision": "e6c64098ca1a56bdd156",
+    "url": "/powerbet/static/css/main.bd7bcf54.chunk.css"
+  },
+  {
+    "revision": "2652880592d22e16e56d",
+    "url": "/powerbet/static/js/2.bdc4a500.chunk.js"
+  },
+  {
+    "revision": "41f664ab3b67ffbc80684810068df507",
+    "url": "/powerbet/static/js/2.bdc4a500.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c64098ca1a56bdd156",
+    "url": "/powerbet/static/js/main.8d9206ba.chunk.js"
+  },
+  {
+    "revision": "72b45df32db1945bda8a",
+    "url": "/powerbet/static/js/runtime-main.003158be.js"
+  },
+  {
+    "revision": "7ed3212571af16d7f704d2a4c8488468",
+    "url": "/powerbet/static/media/fire.7ed32125.png"
+  },
+  {
+    "revision": "92d20dd00ee7cdc7dc9b778757dfb542",
+    "url": "/powerbet/static/media/me.92d20dd0.png"
+  }
+]);
